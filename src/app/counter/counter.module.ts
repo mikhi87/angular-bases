834cf561ab1093a4e21e77bc83d35f5d6f5ef152
mikhi87 <garciavidal.miguelangel@gmail.com>
2025-02-12 17:27:41 +0100

    Fin de sección 4

---
+++ src/app/counter/counter.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { CounterComponent } from "./components/counter/counter.component";
+
+
+@NgModule({
+  exports: [
+    CounterComponent
+  ],
+  declarations: [
+    CounterComponent
+  ]
+})
+export class CounterModule {
+
+}
